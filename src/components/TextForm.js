8ee35e6ca218640e51bb1d17214b58f79ece1594
Om Khatri <omkhatri2929@gmail.com>
2024-01-15 22:19:00 +0530

    final changes-1

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const[text,setText]=useState("enter the text ")
+    const[text,setText]=useState("")
     const handleeOnClick1=(e)=>{
         e.preventDefault()
         let upText=text.toUpperCase()
@@ -39,12 +39,12 @@ export default function TextForm(props) {
  
   <h1>{props.heading}</h1>
   <div className="form-group">
-    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.theme==="light"?"white":"grey" ,color:props.theme==="light"?"black":"white" }}id="mybox" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Enter the text"style={{backgroundColor:props.theme==="light"?"white":"grey" ,color:props.theme==="light"?"black":"white" }}id="mybox" rows="8"></textarea>
   </div>
-  <button className="btn btn-primary my-1" id="myButton1"onClick={handleeOnClick1}>Convert to uppercase</button>
-  <button className="btn btn-primary mx-2" id="myButton1"onClick={handleeOnClick2}>Convert to lowercase</button>
-  <button className="btn btn-primary" id="myButton2"onClick={handleeOnClick3}>Remove Extra space</button>
-  <button className="btn btn-danger mx-2" id="myButton2"onClick={handleeOnClick4}>clear</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" id="myButton1"onClick={handleeOnClick1}>Convert to uppercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" id="myButton1"onClick={handleeOnClick2}>Convert to lowercase</button>
+  <button disabled={text.split(" ").filter((word)=>word==="").length<2} className="btn btn-primary mx-1 my-1" id="myButton2"onClick={handleeOnClick3}>Remove Extra space</button>
+  <button  disabled={text.length===0}className="btn btn-danger mx-1 my-1" id="myButton2"onClick={handleeOnClick4}>clear</button>
 
 </form>
  <div className="container my-3">
@@ -52,7 +52,7 @@ export default function TextForm(props) {
   <p>{text.split(" ").filter((word)=>word!=="").length} words and {text.length} characters</p>
   <p>{0.008*text.split(" ").filter((word)=>word!=="").length} Minutes read</p>
   <h2>Preview</h2>
-  <p>{text.length>0?text:"enter the text in the text area to preview it here"}</p>
+  <p>{text.length>0?text:"Nothing to preview!"}</p>
  </div>
     </div>
    
