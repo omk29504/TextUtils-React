cf42c1adeedd6f595f1d81e8bda956c02ccc8bc1
Om Khatri <omkhatri2929@gmail.com>
2024-01-16 21:47:49 +0530

    final changes-2

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,18 +39,18 @@ export default function TextForm(props) {
  
   <h1>{props.heading}</h1>
   <div className="form-group">
-    <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Enter the text"style={{backgroundColor:props.theme==="light"?"white":"grey" ,color:props.theme==="light"?"black":"white" }}id="mybox" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Enter the text"style={{backgroundColor:props.theme==="light"?"white":"#1c2731" ,color:props.theme==="light"?"black":"white" }}id="mybox" rows="8"></textarea>
   </div>
   <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" id="myButton1"onClick={handleeOnClick1}>Convert to uppercase</button>
   <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" id="myButton1"onClick={handleeOnClick2}>Convert to lowercase</button>
-  <button disabled={text.split(" ").filter((word)=>word==="").length<2} className="btn btn-primary mx-1 my-1" id="myButton2"onClick={handleeOnClick3}>Remove Extra space</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" id="myButton2"onClick={handleeOnClick3}>Remove Extra space</button>
   <button  disabled={text.length===0}className="btn btn-danger mx-1 my-1" id="myButton2"onClick={handleeOnClick4}>clear</button>
 
 </form>
  <div className="container my-3">
   <h2>Your text summary</h2>
-  <p>{text.split(" ").filter((word)=>word!=="").length} words and {text.length} characters</p>
-  <p>{0.008*text.split(" ").filter((word)=>word!=="").length} Minutes read</p>
+  <p>{text.split(/\s+/).filter((word)=>word!=="").length} words and {text.length} characters</p>
+  <p>{0.008*text.split(/\s+/).filter((word)=>word!=="").length} Minutes read</p>
   <h2>Preview</h2>
   <p>{text.length>0?text:"Nothing to preview!"}</p>
  </div>
